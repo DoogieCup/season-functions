47a9cc3cfa0af600e9d5dbf66bb83e9eaa3991b0
Simon Duff <simon.j.duff@gmail.com>
2017-03-03 16:36:11 +0000

    Further event sourcing

--- command_handler/season.js
+++ command_handler/season.js
@@ -2,12 +2,32 @@
 
 (function(){
     module.exports = class{
-        constructor(log){
+        constructor(log, events){
             this.log = log;
+            this.rounds = [];
+
+            events.forEach(function(event) {
+                this.apply(event);
+            }, this);
         }
 
         apply(event) {
+            switch (event.eventType){
+                case 'seasonCreated':
+                    this.applySeasonCreated(event);
+                break;
+                case 'roundAdded':
+                    this.applyRoundCreated(event);
+                break;
+            }
+        };
+
+        applySeasonCreated(event){
             this.Id = event.year;
         };
+
+        applyRoundCreated(event){
+            this.rounds.push({round:event.round});
+        }
     }
 })();
--- test/season_tests.js
+++ test/season_tests.js
@@ -5,19 +5,34 @@
     
     var Season = require('../command_handler/season.js');
 
+    var log = (msg) => {console.log(msg);}
+
+    function e(name, event){
+        event.eventType = name;
+        return event;
+    }
+
     tape('Constructor does now throw', (t) => {
         t.plan(1);
-        t.doesNotThrow(() => {var season = new Season();});
+        t.doesNotThrow(() => {var season = new Season(log, []);});
     });
 
     tape('Season created', (t) => {
-        var season = new Season();
-        var event = {eventType: 'seasonCreated', year:2016}
-
-        season.apply(event);
+        var event = e('seasonCreated', {year:2016});
+        var season = new Season(log, [event]);
 
         t.equal(season.Id, 2016);
 
         t.end();
+    });
+
+    tape('Round added', (t) => {
+        var events = [e('seasonCreated', {year:2016}),
+            e('roundAdded', {round:1})]
+        var season = new Season(log, events);
+
+        t.equal(1, season.rounds.length);
+        t.equal(1, season.rounds[0].round);
+        t.end(); 
     });
 })();
