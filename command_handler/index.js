5fba5b9cf5762f048dd6479696d1adaf994f2c55
Simon Duff <simon.j.duff@gmail.com>
2017-03-06 11:46:13 +0000

    Testing table integration

--- command_handler/index.js
+++ command_handler/index.js
@@ -1,5 +1,21 @@
 (function(){
     module.exports = function(context) {
-    
+
+        let azure = require('../azure-storage');
+
+        let connectionString = process.env.AzureWebJobsDashboard;
+        let tableService = azure.createTableService(connectionString);
+
+        var query = new azure.TableQuery()
+            .top(5)
+            .where('PartitionKey eq ?', 2008);
+
+        tableService.queryEntities('SeasonEvents', query, null, function(error, result, response) {
+            if (error) {
+                throw Error(error);
+            }
+
+            context.log('Results: ' + JSON.stringify(result));
+        });
     };
 })();
