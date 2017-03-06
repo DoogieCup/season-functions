5154193ea5c0c9b1e77aa26597c8ff1c6b22babf
Simon Duff <simon.j.duff@gmail.com>
2017-03-06 12:13:54 +0000

    Basic integration with azure storage

--- command_handler/index.js
+++ command_handler/index.js
@@ -1,14 +1,14 @@
 (function(){
     module.exports = function(context) {
 
-        let azure = require('../azure-storage');
+        let azure = require('azure-storage');
 
         let connectionString = process.env.AzureWebJobsDashboard;
         let tableService = azure.createTableService(connectionString);
 
         var query = new azure.TableQuery()
             .top(5)
-            .where('PartitionKey eq ?', 2008);
+            .where('PartitionKey eq ?', '2008');
 
         tableService.queryEntities('SeasonEvents', query, null, function(error, result, response) {
             if (error) {
@@ -16,6 +16,8 @@
             }
 
             context.log('Results: ' + JSON.stringify(result));
+
+            context.done();
         });
     };
 })();
