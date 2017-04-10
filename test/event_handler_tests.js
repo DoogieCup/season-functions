a77c070ff1288307ee8d0ecdf4abb319ef4a4b47
Simon Duff <simon.j.duff@gmail.com>
2017-04-10 17:39:01 +0100

    Adding events to read model

--- test/event_handler_tests.js
+++ test/event_handler_tests.js
@@ -5,6 +5,8 @@
 
     var log = (msg) => {console.log(msg);}
 
+    var createEvents = require('./eventBuilder.js')(log);
+
     var EventHandler = require('../event_handler/handler.js');
     
     tape('Constructor does not throw', (t) => {
@@ -15,9 +17,13 @@
     });
 
     tape('When new event, updates read model', (t) => {
+        var events = createEvents([
+            {name:'seasonCreated', event:{year:2016}},
+            {name:'roundAdded', event:{round:1}}]);
+
         var fetcher = sinon.stub().returns({
         });
-        
+
         t.end();
     });
 })();
